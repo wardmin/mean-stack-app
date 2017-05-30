3ac47357c5f07a67da8070d3a54f4cb6fcc0ae64
Ward Minnis <ward@minnis.net>
2017-05-30 12:43:15 +0000

    added flex grid for images and bootstrap styling

--- YelpCamp/v1/app.js
+++ YelpCamp/v1/app.js
@@ -9,6 +9,9 @@ app.set("view engine", "ejs");
 var campgrounds = [
             { name: "Shits Creek", image: "https://farm7.staticflickr.com/6082/6142484013_74e3f473b9.jpg"},
             { name: "Granite Hill", image: "https://farm1.staticflickr.com/756/21043112059_788cbc12ed.jpg"},
+            { name: "Mountain Goat's Rest", image: "https://farm6.staticflickr.com/5786/20607281024_5c7b3635cc.jpg"},
+            { name: "Shits Creek", image: "https://farm7.staticflickr.com/6082/6142484013_74e3f473b9.jpg"},
+            { name: "Granite Hill", image: "https://farm1.staticflickr.com/756/21043112059_788cbc12ed.jpg"},
             { name: "Mountain Goat's Rest", image: "https://farm6.staticflickr.com/5786/20607281024_5c7b3635cc.jpg"}
             ];
             
